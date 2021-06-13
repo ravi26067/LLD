795cb2f197149fd819a33820cd53add038da9c03
Ravi Kumar <ravi26067@gmail.com>
2021-06-13 23:06:32 +0530

    Create Chess.java

---
+++ LowLevel/Chess.java
@@ -0,0 +1,146 @@
+class Chess{
+	ChessBoard board;
+	Player[] players;
+	Player currentPlayer;
+	List<Move> moves;
+	GameStatus status;
+
+
+	public boolean playMove(CellPosition start,CellPosition end,Piece piece);
+	public boolean endGame();
+	private void changeTurn();
+}
+
+class ChessBoard{
+	List<List<Cell>> cells;
+
+	public void resetBoard();
+	public void updateBoard(Move move);
+
+}
+
+class Cell{
+	Color color;
+	Piece piece;
+	CellPosition position;
+}
+
+clas CellPosition{
+	Char col;
+	int row;
+}
+
+class Player{
+	Account account;
+	Color color;
+	Time tileleft;
+
+}
+
+
+class Move{
+	Player turn;
+	Piece piece;
+	Piece killedPiece;
+	CellPosition start;
+	CellPosition end;
+}
+
+class Account{
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private String phone;
+}
+
+enum GameStatus{
+	BLACK_WIN,
+	WHITE_WIN, 
+	ACTIVE,
+	PAUSED
+}
+
+enum Color{
+	BLACK,
+	WHITE
+}
+
+class Time{
+	int mins;
+	int secs;
+}
+
+abstract class Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class Bishop extends Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class Queen Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class King Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class Rook Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class Knight Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+class Pawn Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+abstract class Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+abstract class Piece{
+	Color color;
+	public abstract boolean move(CellPosition from , CellPosition toPosition);
+	public abstract List<CellPosition> possibleMoves(CellPosition start);
+	public abstract boolean validateMove(CellPosition start,CellPosition end);
+
+}
+
+
